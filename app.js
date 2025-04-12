f8b8cde504ca7b148f5ad1b2d6e7aed745dbcf83
Bikash Kumar Choudhury <kumarbikashchoudhury2004@gmail.com>
2025-04-12 16:07:20 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+const app = express();
+import cookieParser from 'cookie-parser';
+import connectDB from './db/db.js';
+connectDB();
+import userRoutes from './routes/user.route.js';
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Welcome')
+})
+app.use('/users', userRoutes);
+
+
+export default app;
+
---
+++ controllers/user.controller.js
@@ -0,0 +1,67 @@
+import userModel from "../models/user.model.js";
+import { createUser } from "../services/user.services.js";
+import { validationResult } from "express-validator";
+import BlacklistedToken from "../models/blacklistToken.Model.js";
+
+export const registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { fullname, email, password } = req.body;
+
+  const isUserAlreadyExist = await userModel.findOne({ email });
+
+  if(isUserAlreadyExist){
+    return res.status(400).json({ message: "User already exist"});
+  }
+
+  const hashedPassword = await userModel.hashPassword(password);
+
+  const user = await createUser({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
+  });
+  const token = user.generateAuthToken();
+  res.status(201).json({ token, user });
+};
+
+export const loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({email}).select('+password');
+
+  if(!user){
+    return res.status(401).json({ message: 'Invalid email or password'})
+  }
+
+  const isMatch = await user.comparePassword(password)
+
+  if(!isMatch){
+    return res.status(401).json({ message: 'Invalid email or password'})
+  }
+  const token = user.generateAuthToken();
+
+  res.cookie('token', token );
+  res.status(200).json({token, user})
+}
+
+export const getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+}
+
+export const logoutUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+  
+  await BlacklistedToken.create({ token });
+  res.clearCookie('token');
+
+  res.status(200).json({ message: 'Logged out'});
+}
